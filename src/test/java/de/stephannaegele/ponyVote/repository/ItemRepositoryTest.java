5ae76322f85142a04c0589b05c6df7524d9480ca
Stephan <stephan.naegele@gmail.com>
2017-10-27 14:52:26 +0200

    Test for circleci-Detection

---
+++ src/test/java/de/stephannaegele/ponyVote/repository/ItemRepositoryTest.java
@@ -0,0 +1,15 @@
+package de.stephannaegele.ponyVote.repository;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ItemRepositoryTest {
+
+    @Test
+    public void Test() throws Exception{
+        assertTrue(4L == new Long(4));
+    }
+
+
+}
